d4e4e6442501f7854947724bcfdf4b432e13d5f9
FernandoFelberque <129217816+FernandoFelberque@users.noreply.github.com>
2023-06-05 13:18:46 -0300

    Update SomaDeMultiplos.js

--- Tarefas_Docs/AulaJavaExercicios/SomaDeMultiplos.js
+++ Tarefas_Docs/AulaJavaExercicios/SomaDeMultiplos.js
@@ -1,13 +1,13 @@
-function somaMultiplos() {
+function somaMultiplos(a ,b) {
     let soma = 0;
   
     for (let i = 1; i < 1000; i++) {
-      if (i % 5 === 0 || i % 7 === 0) {
+      if (i % a === 0 || i % b === 0) {
         soma += i;
       }
     }
   
     return soma;
   }
-  
+  module.exports = {somaMultiplos}
   console.log(somaMultiplos()); 
